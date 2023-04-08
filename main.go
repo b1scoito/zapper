eb0cc8a5830ee8ce0e64d3278397cc78519a6744
biscoito <biscoito@riseup.net>
2023-04-08 05:20:03 -0300

    fix order of stuff

--- main.go
+++ main.go
@@ -80,20 +80,6 @@ func main() {
 	// Use all CPU cores for parallelism
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	var groupsSend string
-	var groupsReceive string
-
-	flag.StringVar(&groupsSend, "sends", "", "List of groups to send messages to, separated by a comma. (e.g. 120363122182986428,120363120553285556)")
-	flag.StringVar(&groupsReceive, "receives", "", "List of groups to receive messages from, separated by a comma. (e.g. 120363122182986428,120363120553285556)")
-	flag.Parse()
-
-	// If argument is null, exit
-	if len(groupsSend) <= 0 || len(groupsReceive) <= 0 {
-		fmt.Printf("Usage: %s -sends (jid,jid) -receives (jid,jid)\n", filepath.Base(os.Args[0]))
-		flag.PrintDefaults()
-		os.Exit(0)
-	}
-
 	dbLog := waLog.Stdout("Database", "INFO", true)
 	// Make sure you add appropriate DB connector imports, e.g. github.com/mattn/go-sqlite3 for SQLite
 	container, err := sqlstore.New("sqlite3", "file:wadata.db?_foreign_keys=on", dbLog)
@@ -109,12 +95,6 @@ func main() {
 
 	clientLog := waLog.Stdout("Client", "INFO", true)
 	client := whatsmeow.NewClient(deviceStore, clientLog)
-
-	cli := &RedirectMessages{WMClient: client}
-	cli.registerEvents()
-	cli.groupsToSend = strings.Split(groupsSend, ",")
-	cli.groupsToReceive = strings.Split(groupsReceive, ",")
-
 	if client.Store.ID == nil {
 		// No ID stored, new login
 		qrChan, err := client.GetQRChannel(context.Background())
@@ -128,7 +108,7 @@ func main() {
 
 		for evt := range qrChan {
 			if evt.Event == "code" {
-				fmt.Println("QR code:", evt.Code)
+				fmt.Println("Scan the following QR Code on the WhatsApp app to login:") // evt.Code
 				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
 			} else {
 				fmt.Println("Login event:", evt.Event)
@@ -140,6 +120,25 @@ func main() {
 			panic(err)
 		}
 	}
+
+	var groupsSend string
+	var groupsReceive string
+
+	flag.StringVar(&groupsSend, "sends", "", "List of groups to send messages to, separated by a comma. (e.g. 120363122182986428,120363120553285556)")
+	flag.StringVar(&groupsReceive, "receives", "", "List of groups to receive messages from, separated by a comma. (e.g. 120363122182986428,120363120553285556)")
+	flag.Parse()
+
+	// If argument is null, exit
+	if len(groupsSend) <= 0 || len(groupsReceive) <= 0 {
+		fmt.Printf("Usage: %s -sends (jid,jid) -receives (jid,jid)\n", filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	cli := &RedirectMessages{WMClient: client}
+	cli.registerEvents()
+	cli.groupsToSend = strings.Split(groupsSend, ",")
+	cli.groupsToReceive = strings.Split(groupsReceive, ",")
 
 	// List all joined groups and their respective JIDs
 	groups, err := client.GetJoinedGroups()
