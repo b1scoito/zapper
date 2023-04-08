f8412381cb35273ce6e1822f2836d79b6b103727
biscoito <biscoito@riseup.net>
2023-04-08 05:06:16 -0300

    add missing close parenthesis

--- main.go
+++ main.go
@@ -151,7 +151,7 @@ func main() {
 		fmt.Printf("Group: %s ID: %s\n", group.Name, group.JID.User)
 	}
 
-	fmt.Println("Running... (Press Ctrl+C to exit")
+	fmt.Println("Running... (Press Ctrl+C to exit)")
 
 	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
 	c := make(chan os.Signal, 1)
